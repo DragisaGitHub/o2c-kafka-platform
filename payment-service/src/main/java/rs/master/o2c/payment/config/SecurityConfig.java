d860ab30f598de491f400c61ab0a294b146e838d
PRODYNA\dstankovski <dragisa.stankovski@prodyna.com>
2026-01-01 21:42:25 +0100

    chore(payment): add local CORS for frontend dev server

--- payment-service/src/main/java/rs/master/o2c/payment/config/SecurityConfig.java
+++ payment-service/src/main/java/rs/master/o2c/payment/config/SecurityConfig.java
@@ -1,4 +1,21 @@
 package rs.master.o2c.payment.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
 public class SecurityConfig {
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+				.csrf(ServerHttpSecurity.CsrfSpec::disable)
+				.cors(cors -> {})
+				.authorizeExchange(ex -> ex
+						.pathMatchers("/actuator/**").permitAll()
+						.anyExchange().permitAll()
+				)
+				.build();
+	}
 }
