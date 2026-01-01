870e9c94dca54f5acb322571842df2f46254ef8b
PRODYNA\dstankovski <dragisa.stankovski@prodyna.com>
2026-01-01 21:55:27 +0100

    feat(observability): propagate X-Correlation-Id across services

---
+++ common-events/src/main/java/rs/master/o2c/events/CorrelationHeaders.java
@@ -0,0 +1,8 @@
+package rs.master.o2c.events;
+
+public final class CorrelationHeaders {
+	public static final String X_CORRELATION_ID = "X-Correlation-Id";
+
+	private CorrelationHeaders() {
+	}
+}
