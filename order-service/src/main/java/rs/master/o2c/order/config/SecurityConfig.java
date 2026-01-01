5b05549f291132ab2a97a0d083246c587ba2f824
PRODYNA\dstankovski <dragisa.stankovski@prodyna.com>
2026-01-01 21:35:27 +0100

    chore(order): add local CORS for frontend dev server

--- order-service/src/main/java/rs/master/o2c/order/config/SecurityConfig.java
+++ order-service/src/main/java/rs/master/o2c/order/config/SecurityConfig.java
@@ -12,6 +12,7 @@ public class SecurityConfig {
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+            .cors(cors -> {})
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers("/orders/**").permitAll()
                         .pathMatchers("/actuator/**").permitAll()
